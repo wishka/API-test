c2a4e01e28e1702e4e776dd320e3fb6fb89aba6d
wishka <roucke@mail.ru>
2020-05-07 17:02:48 +0300

    add jbuilder and serializer

--- app/controllers/API/v1/posts_controller.rb
+++ app/controllers/API/v1/posts_controller.rb
@@ -1,6 +1,9 @@
 module Api
     module V1
         class PostsController < ApplicationController
+          def index
+            @posts = Post.order('created_at DESC')
+          end
         end
     end
 end
