90e59fd3974ffd4d06b4e7f68a220cc7f1262a59
wishka <roucke@mail.ru>
2020-05-07 17:24:44 +0300

    add user view

--- app/controllers/API/v1/users_controller.rb
+++ app/controllers/API/v1/users_controller.rb
@@ -1,6 +1,19 @@
 module Api
     module V1
       class UsersController < ApplicationController
+        def create
+          if @user.save
+          render status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name)
+      end
       end
     end
 end
